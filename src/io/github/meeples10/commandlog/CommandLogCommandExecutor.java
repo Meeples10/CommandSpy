c5053618fdd6ce6266998503b43bb6ee18adcb79
Chase <cvaughan011@gmail.com>
2018-06-07 00:46:13 -0400

    Initial commit

---
+++ src/io/github/meeples10/commandlog/CommandLogCommandExecutor.java
@@ -0,0 +1,240 @@
+package io.github.meeples10.commandlog;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class CommandLogCommandExecutor implements CommandExecutor {
+
+    public CommandLogCommandExecutor() {
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("commandlog")) {
+            if(args.length == 0) {
+                sender.sendMessage("§aCommandLog §fversion §a"
+                        + Bukkit.getServer().getPluginManager().getPlugin(Main.NAME).getDescription().getVersion());
+                sender.sendMessage("§fBy §aMeeples10");
+                sender.sendMessage("§fType §a/cl help §ffor more information");
+                return true;
+            }
+            
+            if((args[0].equalsIgnoreCase("reload")) || (args[0].equalsIgnoreCase("rl"))) {
+                return reloadCommand(sender, cmd, label, args);
+            }
+
+            if((args[0].equalsIgnoreCase("disable")) || (args[0].equalsIgnoreCase("d"))) {
+                return disableCommand(sender, cmd, label, args);
+            }
+
+            if((args[0].equalsIgnoreCase("enable")) || (args[0].equalsIgnoreCase("e"))) {
+                return enableCommand(sender, cmd, label, args);
+            }
+
+            if((args[0].equalsIgnoreCase("history")) || (args[0].equalsIgnoreCase("h"))) {
+                return historyCommand(sender, cmd, label, args);
+            }
+
+            if((args[0].equalsIgnoreCase("help")) || (args[0].equalsIgnoreCase("?"))) {
+                return helpCommand(sender, cmd, label, args);
+            }
+        }
+
+        return false;
+    }
+
+    private boolean reloadCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if((sender instanceof Player)) {
+            Player player = (Player) sender;
+
+            if(player.hasPermission("commandlog.admin")) {
+                Bukkit.getServer().getPluginManager().getPlugin(Main.NAME).reloadConfig();
+                Main.loadConfig();
+                Main.log.info("[" + Main.NAME + "] Config reloaded");
+                sender.sendMessage("§a[§eCL§a] §fConfig reloaded");
+            } else {
+                sender.sendMessage(ChatColor.RED + "Insufficient permissions");
+            }
+        } else {
+            Bukkit.getServer().getPluginManager().getPlugin(Main.NAME).reloadConfig();
+            Main.loadConfig();
+            Main.log.info("[" + Main.NAME + "] Config reloaded");
+        }
+        return true;
+    }
+
+    private boolean disableCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p;
+        if(Main.allowDisable()) {
+            if((sender instanceof Player)) {
+                Player player = (Player) sender;
+
+                if(player.hasPermission("commandlog.admin")) {
+                    Main.setNotifications(false);
+
+                    List<Player> playerList = (List<Player>) Bukkit.getServer().getOnlinePlayers();
+                    if(playerList != null) {
+                        for(Iterator<Player> localIterator = playerList.iterator(); localIterator.hasNext();) {
+                            p = (Player) localIterator.next();
+                            if(p.hasPermission("commandlog.notice")) {
+                                p.sendMessage(Main.getChatPrefix() + ChatColor.RESET + player.getDisplayName()
+                                        + ChatColor.RESET + " has disabled notifications");
+                            }
+                        }
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Insufficient permissions");
+                }
+            } else {
+                Main.setNotifications(false);
+
+                List<Player> playerList = (List<Player>) Bukkit.getServer().getOnlinePlayers();
+                if(playerList != null) {
+                    for(Player p1 : playerList) {
+                        if(p1.hasPermission("commandlog.notice")) {
+                            p1.sendMessage(
+                                    Main.getChatPrefix() + ChatColor.RESET + " Console has disabled notifications");
+                        }
+                    }
+                }
+                sender.sendMessage(Main.getChatPrefix() + ChatColor.RESET + " Notifications are now disabled");
+            }
+        } else {
+            sender.sendMessage(Main.getChatPrefix() + ChatColor.RESET + " Notifications cannot be disabled");
+        }
+        return true;
+    }
+
+    private boolean enableCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p;
+        if(Main.allowDisable()) {
+            if((sender instanceof Player)) {
+                Player player = (Player) sender;
+
+                if(player.hasPermission("commandlog.admin")) {
+                    Main.setNotifications(true);
+
+                    List<Player> playerList = (List<Player>) Bukkit.getServer().getOnlinePlayers();
+                    if(playerList != null) {
+                        for(Iterator<Player> localIterator = playerList.iterator(); localIterator.hasNext();) {
+                            p = (Player) localIterator.next();
+                            if(p.hasPermission("commandlog.notice")) {
+                                p.sendMessage(Main.getChatPrefix() + ChatColor.RESET + player.getDisplayName()
+                                        + ChatColor.RESET + " has enabled notifications");
+                            }
+                        }
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Insufficient permissions");
+                }
+            } else {
+                Main.setNotifications(true);
+
+                List<Player> playerList = (List<Player>) Bukkit.getServer().getOnlinePlayers();
+                if(playerList != null) {
+                    for(Player p2 : playerList) {
+                        if(p2.hasPermission("commandlog.notice")) {
+                            p2.sendMessage(
+                                    Main.getChatPrefix() + ChatColor.RESET + " Console has enabled notifications");
+                        }
+                    }
+                }
+                sender.sendMessage(Main.getChatPrefix() + ChatColor.RESET + " Notifications are now disabled");
+            }
+        }
+        return true;
+    }
+
+    private boolean historyCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if((sender instanceof Player)) {
+            Player player = (Player) sender;
+
+            if(player.hasPermission("commandlog.history")) {
+                int numHistory = 10;
+                if(args.length == 2) {
+                    try {
+                        numHistory = Integer.parseInt(args[1]);
+                    } catch(NumberFormatException e) {
+                        sender.sendMessage(ChatColor.RED + "Please type a number, for example: " + ChatColor.DARK_RED
+                                + "/cl history 10");
+                        return true;
+                    }
+                }
+                if(numHistory > Main.getCommandHistory().size()) {
+                    numHistory = Main.getCommandHistory().size();
+                }
+
+                sender.sendMessage(Main.getChatPrefix() + ChatColor.GOLD + "Here are the last " + ChatColor.RED
+                        + numHistory + ChatColor.GOLD + " commands:");
+                for(int iHistory = numHistory - 1; iHistory >= 0; iHistory--) {
+                    HistoryItem hi = (HistoryItem) Main.getCommandHistory().get(iHistory);
+                    sender.sendMessage("[" + FormatDateTime("y/M/d HH:mm", hi.getDate()) + "] " + hi.getSender()
+                            + " executed " + hi.getCommand());
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Insufficient permissions");
+            }
+        } else {
+            int numHistory = 10;
+            if(args.length == 2) {
+                try {
+                    numHistory = Integer.parseInt(args[1]);
+                } catch(NumberFormatException e) {
+                    sender.sendMessage(ChatColor.RED + "Please type a number, for example: " + ChatColor.DARK_RED
+                            + "/cl history 10");
+                    return true;
+                }
+            }
+            if(numHistory > Main.getCommandHistory().size()) {
+                numHistory = Main.getCommandHistory().size();
+            }
+
+            sender.sendMessage(Main.getChatPrefix() + "§6Here are the last " + ChatColor.RED + numHistory
+                    + ChatColor.GOLD + " commands:");
+            for(int iHistory = numHistory - 1; iHistory >= 0; iHistory--) {
+                HistoryItem hi = (HistoryItem) Main.getCommandHistory().get(iHistory);
+                sender.sendMessage("[" + FormatDateTime("y/M/d HH:mm", hi.getDate()) + "] " + hi.getSender()
+                        + " executed " + hi.getCommand());
+            }
+        }
+        return true;
+    }
+
+    private boolean helpCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if((sender instanceof Player)) {
+            Player player = (Player) sender;
+
+            if(player.hasPermission("commandlog.notice")) {
+                sender.sendMessage(ChatColor.GOLD + "CommandLog commands:");
+                sender.sendMessage("§c/cl history [x]| hi [x] §f: Shows the last <x> commands executed, default = 10");
+            }
+
+            if(player.hasPermission("commandlog.admin")) {
+                sender.sendMessage("§6CommandLog Admin commands:");
+                sender.sendMessage("§c/cl reload | rl §f: Reload the configuration");
+                sender.sendMessage("§c/cl disable | d §f: Disables command notifications for everyone");
+                sender.sendMessage("§c/cl enable | e §f: Enables command notifications for everyone");
+            }
+        } else {
+            sender.sendMessage("§6CommandLog commands:");
+            sender.sendMessage("§c/cl history <x>| h <x> §f: Shows the last <x> commands executed, default = 10");
+            sender.sendMessage("§6CommandLog Admin commands:");
+            sender.sendMessage("§c/cl reload | rl §f: Reload the configuration");
+            sender.sendMessage("§c/cl disable | d §f: Disables command notifications for everyone");
+            sender.sendMessage("§c/cl enable | e §f: Enables command notifications for everyone");
+        }
+        return true;
+    }
+
+    public String FormatDateTime(String format, java.util.Date ts) {
+        java.text.SimpleDateFormat sformat = new java.text.SimpleDateFormat(format);
+        return sformat.format(ts);
+    }
+}
---
+++ src/io/github/meeples10/commandlog/Main.java
@@ -0,0 +1,174 @@
+package io.github.meeples10.commandlog;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public final class Main extends JavaPlugin implements Listener {
+    public static final String NAME = "CommandLog";
+
+    private static String chatFormat = "";
+    private static String chatPrefix = "";
+    private static boolean enableNotifications = true;
+    private static boolean enableFileLog = true;
+    private static List<HistoryItem> commandHistory;
+    private static boolean allowDisable;
+    private static File df, cfg;
+    public static Logger log;
+
+    public void onEnable() {
+        df = Bukkit.getServer().getPluginManager().getPlugin(NAME).getDataFolder();
+        cfg = new File(df, "config.yml");
+        log = Bukkit.getServer().getPluginManager().getPlugin(NAME).getLogger();
+        getServer().getPluginManager().registerEvents(this, this);
+        loadConfig();
+        getCommand("commandlog").setExecutor(new CommandLogCommandExecutor());
+        commandHistory = new ArrayList<HistoryItem>();
+    }
+
+    public static void loadConfig() {
+        Bukkit.getServer().getPluginManager().getPlugin(NAME).saveDefaultConfig();
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(cfg);
+
+        chatPrefix = ChatColor.translateAlternateColorCodes('&', config.getString("chatprefix"));
+        chatFormat = ChatColor.translateAlternateColorCodes('&', config.getString("chatformat"));
+
+        allowDisable = config.getBoolean("allowdisable");
+
+        int pIndex = -1;
+        int cIndex = -1;
+        if(chatFormat.length() > 0) {
+            pIndex = chatFormat.indexOf("{Player}");
+            cIndex = chatFormat.indexOf("{Command}");
+        }
+        if((pIndex < 0) || (cIndex < 0)) {
+            log.warning("chatformat string must at least contain {Player} and {Command}");
+            log.warning("Using default chatformat string now");
+
+            MemoryConfiguration memconfig = (MemoryConfiguration) config.getDefaults();
+            chatFormat = memconfig.getString("chatformat");
+        }
+
+        enableFileLog = config.getBoolean("filelog");
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if(!enableNotifications) {
+            return;
+        }
+        String s = event.getMessage();
+        if(s.length() == 0) {
+            return;
+        }
+
+        String[] parts = s.split(" ");
+        if(parts.length == 0) {
+            return;
+        }
+        
+        for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+            if(p.hasPermission("commandlog.notice")) {
+                LogCommandToOnlinePlayer(event, p);
+            }
+        }
+
+        Player player = event.getPlayer();
+        HistoryItem hi = new HistoryItem(s, player.getDisplayName(), new Date(), player.getLocation());
+        commandHistory.add(hi);
+
+        if(enableFileLog) {
+            LogCommandToFile(hi);
+        }
+    }
+
+    private void LogCommandToOnlinePlayer(PlayerCommandPreprocessEvent event, Player p) {
+        Player player = event.getPlayer();
+        String s = event.getMessage();
+
+        if(p != player) {
+            p.sendMessage(chatPrefix + chatFormat.replace("{Player}", player.getDisplayName()).replace("{Command}", s));
+        }
+    }
+
+    private void LogCommandToFile(HistoryItem hi) {
+        File dataFolder = getDataFolder();
+        if(!dataFolder.exists()) {
+            dataFolder.mkdir();
+        }
+
+        String fileName = "commandlog_" + FormatDateTime("yMMdd", new Date()) + ".txt";
+        File saveTo = new File(getDataFolder(), fileName);
+        if(!saveTo.exists()) {
+            try {
+                saveTo.createNewFile();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            FileWriter fw = new FileWriter(saveTo, true);
+            PrintWriter pw = new PrintWriter(fw);
+
+            pw.println("[" + FormatDateTime("yyyy/MM/dd HH:mm:ss", hi.getDate()) + "] " + hi.getSender() + " executed "
+                    + hi.getCommand() + " at " + hi.getLocation());
+            pw.flush();
+            pw.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String FormatDateTime(String format, Date ts) {
+        SimpleDateFormat sformat = new SimpleDateFormat(format);
+        return sformat.format(ts);
+    }
+
+    public static boolean allowDisable() {
+        return allowDisable;
+    }
+
+    public static String getChatFormat() {
+        return chatFormat;
+    }
+
+    public static String getChatPrefix() {
+        return chatPrefix;
+    }
+
+    public static void setNotifications(boolean state) {
+        enableNotifications = state;
+    }
+
+    public static boolean allowNotifications() {
+        return enableNotifications;
+    }
+
+    public static boolean allowFileLog() {
+        return enableFileLog;
+    }
+
+    public static List<HistoryItem> getCommandHistory() {
+        return commandHistory;
+    }
+}
