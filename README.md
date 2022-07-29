eeec0fbb1ce925ba3799e7189210c8ca28963529
Meeples10 <mc.meeples10@gmail.com>
2022-07-28 20:14:23 -0400

    Change name to CommandSpy

---
+++ README.md
@@ -0,0 +1,3 @@
+# CommandSpy
+
+A plugin that allows server moderators to view the commands used by other players.
--- src/main/java/io/github/meeples10/commandspy/CommandCSpy.java
+++ src/main/java/io/github/meeples10/commandspy/CommandCSpy.java
@@ -1,4 +1,4 @@
-package io.github.meeples10.commandlog;
+package io.github.meeples10.commandspy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,9 +9,9 @@ import org.bukkit.entity.Player;
 import io.github.meeples10.meepcore.MeepCommand;
 import io.github.meeples10.meepcore.Messages;
 
-public class CommandCL extends MeepCommand {
+public class CommandCSpy extends MeepCommand {
 
-    public CommandCL(String usage) {
+    public CommandCSpy(String usage) {
         super(usage);
     }
 
@@ -35,17 +35,17 @@ public class CommandCL extends MeepCommand {
             }
         } else {
             sender.sendMessage(Messages.format("$hl" + Main.NAME + " $t"
-                    + Messages.translate(sender, "command.commandlog.cl.default.version") + " $hl"
+                    + Messages.translate(sender, "command.commandspy.cspy.default.version") + " $hl"
                     + Bukkit.getServer().getPluginManager().getPlugin(Main.NAME).getDescription().getVersion() + "\n$t"
-                    + Messages.translate(sender, "command.commandlog.cl.default.author") + ": $hlMeeples10\n$t"
-                    + String.format(Messages.translate(sender, "command.commandlog.cl.default.help"),
-                            Messages.translate(sender, "command.commandlog.cl.usage"))));
+                    + Messages.translate(sender, "command.commandspy.cspy.default.author") + ": $hlMeeples10\n$t"
+                    + String.format(Messages.translate(sender, "command.commandspy.cspy.default.help"),
+                            Messages.translate(sender, "command.commandspy.cspy.usage"))));
             return true;
         }
     }
 
     private static boolean reloadCommand(CommandSender sender) {
-        if(sender.hasPermission("commandlog.reload")) {
+        if(sender.hasPermission("commandspy.reload")) {
             sender.sendMessage(Messages.reloadAttempt(sender, Main.NAME));
             sender.sendMessage(Messages.reloadMessage(sender, Main.NAME, Main.loadConfig()));
         } else {
@@ -58,17 +58,17 @@ public class CommandCL extends MeepCommand {
         if(Main.allowDisabling()) {
             Main.setNotifications(false);
             for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-                if(p.hasPermission("commandlog.notice")) {
+                if(p.hasPermission("commandspy.notice")) {
                     p.sendMessage(Messages
-                            .format(Messages.translate(p, "commandlog.prefix")
-                                    + Messages.translate(p, "command.commandlog.cl.disabled.success"))
+                            .format(Messages.translate(p, "commandspy.prefix")
+                                    + Messages.translate(p, "command.commandspy.cspy.disabled.success"))
                             .replace("{{PLAYER}}",
                                     sender instanceof Player ? ((Player) sender).getDisplayName() : sender.getName()));
                 }
             }
         } else {
-            sender.sendMessage(Messages.format(Messages.translate(sender, "commandlog.prefix")
-                    + Messages.translate(sender, "command.commandlog.cl.disabled.failure")));
+            sender.sendMessage(Messages.format(Messages.translate(sender, "commandspy.prefix")
+                    + Messages.translate(sender, "command.commandspy.cspy.disabled.failure")));
         }
         return true;
     }
@@ -77,27 +77,27 @@ public class CommandCL extends MeepCommand {
         if(Main.allowDisabling()) {
             Main.setNotifications(true);
             for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-                if(p.hasPermission("commandlog.notice")) {
+                if(p.hasPermission("commandspy.notice")) {
                     p.sendMessage(Messages
-                            .format(Messages.translate(p, "commandlog.prefix")
-                                    + Messages.translate(p, "command.commandlog.cl.enabled.success"))
+                            .format(Messages.translate(p, "commandspy.prefix")
+                                    + Messages.translate(p, "command.commandspy.cspy.enabled.success"))
                             .replace("{{PLAYER}}",
                                     sender instanceof Player ? ((Player) sender).getDisplayName() : sender.getName()));
                 }
             }
         } else {
-            sender.sendMessage(Messages.format(Messages.translate(sender, "commandlog.prefix")
-                    + Messages.translate(sender, "command.commandlog.cl.enabled.failure")));
+            sender.sendMessage(Messages.format(Messages.translate(sender, "commandspy.prefix")
+                    + Messages.translate(sender, "command.commandspy.cspy.enabled.failure")));
         }
         return true;
     }
 
     private static boolean helpCommand(CommandSender sender) {
         sender.sendMessage(Messages.format("$t" + ChatColor.STRIKETHROUGH + "---------------$hl " + Main.NAME
-                + " Help $t" + ChatColor.STRIKETHROUGH + "---------------$hl\n" + "/cl reload$t: "
-                + Messages.translate(sender, "command.meepcore.help.reload") + "$hl\n" + "/cl disable | d$t: "
-                + Messages.translate(sender, "command.commandlog.cl.help.disable") + "$hl\n" + "/cl enable | e$t: "
-                + Messages.translate(sender, "command.commandlog.cl.help.enable")));
+                + " Help $t" + ChatColor.STRIKETHROUGH + "---------------$hl\n" + "/cspy reload$t: "
+                + Messages.translate(sender, "command.meepcore.help.reload") + "$hl\n" + "/cspy disable | d$t: "
+                + Messages.translate(sender, "command.commandspy.cspy.help.disable") + "$hl\n" + "/cspy enable | e$t: "
+                + Messages.translate(sender, "command.commandspy.cspy.help.enable")));
         return true;
     }
 }
